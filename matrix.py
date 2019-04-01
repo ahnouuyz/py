4b8d5671347a9b25930d3209a2575b085a07ecfb
Roscoe <45630405+roscoelai@users.noreply.github.com>
2019-04-01 23:38:14 +0800

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,17 +1,18 @@
 class Matrix:
     def __init__(self, ll):
         self.ll = ll
+        self.nrows = len(ll)
+        self.ncols = len(ll[0])
     
     def __repr__(self):
         sll = [list(map(str, row)) for row in self.ll]
         cws = [max(map(len, col)) for col in zip(*sll)]
-
         txt = ''
         for i, row in enumerate(sll):
-            line = '[[' if i == 0 else ' ['
+            line = ' [' if i > 0 else '[['
             for cw, val in zip(cws, row):
-                pad = (cw - len(val)) * ' '
-                line += pad + val + ' '
+                spaces = (cw - len(val)) * ' '
+                line += spaces + val + ' '
             line = line[:-1]
             line += ']' if i < len(sll) - 1 else ']]'
             txt += line + '\n'
@@ -20,12 +21,26 @@ class Matrix:
     def __str__(self):
         return self.__repr__()
     
-    def __add__(self, val):
-        if isinstance(val, (int, float)):
-            ll = self.ll
-            add_val = lambda a: a + val
-            add_vals = lambda lst: list(map(add_val, lst))
-            return Matrix(list(map(add_vals, ll)))
+    def __add__(self, other):
+        if isinstance(other, (int, float)):
+            add_to_all = lambda lst: list(map(lambda a: a + other, lst))
+            return Matrix(list(map(add_to_all, self.ll)))
+    
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, (int, float)):
+            sub_from_all = lambda lst: list(map(lambda a: a - other, lst))
+            return Matrix(list(map(sub_from_all, self.ll)))
+    
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            mul_to_all = lambda lst: list(map(lambda a: a * other, lst))
+            return Matrix(list(map(mul_to_all, self.ll)))
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
     def __matmul__(self, rm):
         lm = self.ll
@@ -35,34 +50,31 @@ class Matrix:
         if len(lm[0]) == len(rm):
             dot = lambda v1, v2: sum(map(lambda a, b: a * b, v1, v2))
             result = [[dot(lm[r], rm_T[c]) for c in range(len(rm_T))] for r in range(len(lm))]
-            return result
+            return Matrix(result)
         else:
             print('Unable to multiply matrices: ncols(left) != nrows(right).')
             return None
 
     def transpose(self):
         return Matrix(list(zip(*self.ll)))
+    
+    def T(self):
+        return Matrix(list(zip(*self.ll)))
 
 if __name__ == '__main__':
-    A = [[1, 2, 3, 10],
-         [4, 5, 6, 11],
-         [7, 8, 9, 12]]
-
-    B = [[5], 
-         [5],
+    A = [[1, 2],
+         [3, 4],
+         [5, 6]]
+    B = [[1], 
          [2],
-         [1]]
-
+         [3]]
     A = Matrix(A)
     B = Matrix(B)
 
     print(A)
-    print(A + 1)
-    print(A.transpose())
-    print(B)
-    print(B.transpose())
-
-    C = Matrix(A @ B)
-    print(C)
-    D = Matrix(B @ B.transpose())
-    print(D)
+    print(A * 2)
+    print(2 * A)
+    print(A.T())
+    print(B.T() @ A)
+    print(A @ A.T() @ A)
+    print(A @ A.T() @ A + 1)
