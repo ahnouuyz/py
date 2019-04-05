b22cd0823ec2762255d515debb1185d3ae0d1ef5
Roscoe <ahnouuyz@gmail.com>
2019-04-05 19:17:01 +0800

    Fix bugs

--- reversi.py
+++ reversi.py
@@ -1,24 +1,14 @@
 import copy
 
-# Define a vector sum function to add tuples element-wise.
-vsum = lambda v1, v2: tuple(map(lambda a, b: a + b, v1, v2))
-
-# Define a set of all valid directions.
-all_dirs = set((v - 1, h - 1) for h in range(3) for v in range(3))
-all_dirs -= {(0, 0)}
-
-# Define a set of all valid board positions.
-all_pos = set((r, c) for c in range(8) for r in range(8))
-
-# Define a set of currently available positions.
-pos_left = copy.deepcopy(all_pos)
-
-def update_pos_left(pos_left, pos):
+def set_remove(set_, pos):
     """ Remove pos from pos_left.
         We can completely avoid this function if we used classes.
         In fact, try to find a way to get rid of this function.
     """
-    pos_left -= set(pos)
+    set_ -= set(pos)
+
+def set_add(set_, pos):
+    set_.update(set(pos))
 
 
 def new_board():
@@ -27,7 +17,7 @@ def new_board():
     ll[3][4] = 1
     ll[4][3] = 1
     ll[4][4] = 2
-    update_pos_left(pos_left, [(3, 3), (3, 4), (4, 3), (4, 4)])
+    set_remove(pos_left, [(3, 3), (3, 4), (4, 3), (4, 4)])
     return ll
 
 def print_board(board):
@@ -35,7 +25,7 @@ def print_board(board):
     # Concurrently, convert numbers in each row to symbols.
     # The small letters are there to visualize valid positions (remove when before final version).
     # Every entry in sboard would be strings at the end.
-    sboard = [[str(i + 1)] + ['-BWbw'[n] for n in row] for i, row in enumerate(sboard)]
+    sboard = [[str(i + 1)] + ['-BWbw'[n] for n in row] for i, row in enumerate(board)]
     sboard.append(' ,a,b,c,d,e,f,g,h'.split(','))
 
     # Store entire printout in render, then print all at once, instead of line-by-line.
@@ -98,7 +88,6 @@ def valid_moves(board, player):
     vmoves = [p for p in ps for d in ds if e(board, player, p, d)]
 
     # Just to help visualize valid positions (remove before final version).
-    print('Valid moves:', vmoves)
     for pos in vmoves:
         r, c = pos
         board[r][c] = player + 2
@@ -109,7 +98,9 @@ def next_state(board, player, pos):
     if pos in valid_moves(board, player):
         r, c = pos
         board[r][c] = player
-        update_pos_left(pos_left, [pos])
+        # Will need to flip the other player's pieces.
+
+        set_remove(pos_left, [pos])
         return board, (0, 2, 1)[player]
     else:
         print('Invalid move.')
@@ -142,32 +133,40 @@ def tests():
         'enclosing(new_board(), 1, (4, 5), (1, 1))',
         'next_state(new_board(), 1, (4, 5))',
         'valid_moves(next_state(new_board(), 1, (4, 5))[0], 2)',
-        'position('e3')',
-        'position('l1')',
-        'position('a0')',
-        'position('Genghis Khan')'
+        'position("e3")',
+        'position("l1")',
+        'position("a0")',
+        'position("Genghis Khan")'
     ]
     
     expected = [
         ''
     ]
     
-    print('Tests:')
-    for test in test_list:
-        print(test, ':', eval(test))
-    
-#    print('score(new_board()):', score(new_board()))
-#    print('enclosing(new_board(), 1, (4, 5), (0, -1)):', enclosing(new_board(), 1, (4, 5), (0, -1)))
-#    print('enclosing(new_board(), 1, (4, 5), (1, 1)):', enclosing(new_board(), 1, (4, 5), (1, 1)))
-#    print('next_state(new_board(), 1, (4, 5)):')
-#    print_board(next_state(new_board(), 1, (4, 5))[0])
-#    print('valid_moves(next_state(new_board(), 1, (4, 5))[0], 2):')
-#    print(valid_moves(next_state(new_board(), 1, (4, 5))[0], 2))
-#    print(position('e3'))
-#    print(position('l1'))
-#    print(position('a0'))
-#    print(position('Genghis Khan'))
+    print()
+    print('-' * 30, 'Tests:', '-' * 30)
+    for i, test in enumerate(test_list):
+        if i == 3:
+            print(i + 1, test, ':')
+            print_board(next_state(new_board(), 1, (4, 5))[0])
+            set_add(pos_left, [(4, 5)])
+        else:
+            print(i + 1, test, ':', eval(test))
 
 if __name__ == '__main__':
+    # Define a vector sum function to add tuples element-wise.
+    vsum = lambda v1, v2: tuple(map(lambda a, b: a + b, v1, v2))
+
+    # Define a set of all valid directions.
+    all_dirs = set((v - 1, h - 1) for h in range(3) for v in range(3))
+    all_dirs -= {(0, 0)}
+
+    # Define a set of all valid board positions.
+    all_pos = set((r, c) for c in range(8) for r in range(8))
+
+    # Define a set of currently available positions.
+    pos_left = copy.deepcopy(all_pos)
+
     tests()
+
 #    print(sorted(pos_left), len(pos_left))
