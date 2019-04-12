170c74740a6cd421cab4b4654ed778389157e0b0
Roscoe <ahnouuyz@gmail.com>
2019-04-12 17:27:36 +0800

    Added random matrix

--- matrix.py
+++ matrix.py
@@ -1,6 +1,10 @@
+import random as rd
+
 class Matrix:
     """ Convenient packaging for a list of lists of numbers.
         Includes some convenient operations.
+        Short-term goal: 
+            Perform simple linear regression.
     """
     def __init__(self, ll):
         self.ll = [[float(v) for v in row] for row in ll]
@@ -56,32 +60,55 @@ class Matrix:
     def flatten(self):
         return [v for row in self.ll for v in row]
 
-    def g_elimination(self):
+    def gauss(self):
         ll = self.ll
+        det = 1
         r, c = 0, 0
-        while r in range(self.shape[0]) and c in range(self.shape[1]):
+        while r < self.shape[0] - 1 and c < self.shape[1] - 1:
             if max(ll[r:], key=lambda x: abs(x[c]))[c] < 1e-10:
-                # No valid pivots, move to next column.
                 c += 1
                 continue
-            rmax = ll[r:].index(max(ll[r:], key=lambda x: abs(x[c]))) + r
-            print(Matrix(ll[r:]))
-            print(rmax, r)
-            ll[rmax], ll[r] = ll[r], ll[rmax]
+            disp = ll[r:].index(max(ll[r:], key=lambda x: abs(x[c])))
+
+            if disp:
+                ll[r + disp], ll[r] = ll[r], ll[r + disp]
+                det *= -1
+            
             for i in range(r + 1, self.shape[0]):
                 mult = ll[i][c] / ll[r][c]
                 for j in range(c, self.shape[1]):
                     ll[i][j] -= ll[r][j] * mult
+            det *= ll[r][c]
             r += 1
             c += 1
-            print(Matrix(ll))
+
+        # We should have the correct determinant at this point.
+
         return Matrix(ll)
 
-    def gj_elimination(self):
+    def jordan(self):
         raise NotImplementedError
+
+        ll = self.ref.ll
+        r, c = self.shape
+        while r > 1 and c > 1:
+            r -= 1
+            c -= 1
+            for i in range(r - 1, 0, -1):
+                mult = ll[i][c] / ll[r][c]
+                for j in range(c, 0, -1):
+                    ll[i][j] -= ll[r][j] * mult
+        return Matrix(ll)
 
     def determinant(self):
         raise NotImplementedError
+
+        # Sign is incorrect; Need to note how many swaps take place.
+        ll = self.ref.ll
+        prod = 1
+        for i in range(min(self.shape)):
+            prod *= ll[i][i]
+        return prod
 
     def invert(self):
         raise NotImplementedError
@@ -98,11 +125,17 @@ class Matrix:
     def eye(cls, n):
         return Matrix([[(0, 1)[r == c] for c in range(n)] for r in range(n)])
 
+    @classmethod
+    def rand(cls, size=(1, 1), randrange=(0, 10)):
+        m, n = size
+        l, u = randrange
+        return Matrix([[rd.randint(l, u) for c in range(n)] for r in range(m)])
+
     __radd__ = __add__
     __rmul__ = __mul__
     T = property(transpose)
-    ref = property(g_elimination)
-    rref = property(gj_elimination)
+    ref = property(gauss)
+    rref = property(jordan)
     det = property(determinant)
 
     # ========== End of Matrix class ==========
@@ -117,7 +150,10 @@ def main():
     C = Matrix([[1, 2, 3, 4], 
                 [5, 6, 7, 8], 
                 [8, 8, 7, 7], 
-                [5, 4, 3, 2]])
+                [5, 4, 3, 3]])
+    D = Matrix([[2, 4], 
+                [6, 8]])
+    E = Matrix.rand((4, 4))
 
     examples = ['print(A)', 
                 'print(A.T)', 
@@ -132,7 +168,10 @@ def main():
                 'print(3 + Matrix.eye(4))', 
                 'print(A.flatten())', 
                 'print(C)', 
-                'print(C.g_elimination())']
+                'print(C.ref)', 
+                'print(Matrix.rand((3, 3)))', 
+                'print(E)', 
+                'print(E.ref)']
 
     for example in examples:
         print(example)
