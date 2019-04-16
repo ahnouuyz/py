8730034b767543a5178f18585d24cee75a7c1e7f
Roscoe <ahnouuyz@gmail.com>
2019-04-16 23:28:17 +0800

    Restructure qual_auto01.py

--- qual_auto01.py
+++ qual_auto01.py
@@ -1,6 +1,5 @@
 """ Qualtrics is not an ideal data entry tool.
-    It's an experience not to be missed...
-    ... alright, it totally can be missed.
+    It's an experience not to be missed.
 """
 
 import time
@@ -12,7 +11,7 @@ REDCOLOR = (195, 36, 50)
 REDPOS = (1, 80)
 TIMEOUT = 10
 
-def wait():
+def wait(delay=0.5):
     """ Need to find an event that is unique to completing a page load.
     """
     start_time = time.time()
@@ -20,17 +19,11 @@ def wait():
         elapsed_time = time.time() - start_time
         if elapsed_time > TIMEOUT:
             raise TimeoutError('Timeout on wait().')
-    time.sleep(0.5)
+    time.sleep(delay)
 
 def next_page():
     pag.click(100, 100)
     for i in range(4):
-        pag.hotkey('shift', '\t')
-    pag.press('enter')
-
-def prev_page():
-    pag.click(100, 100)
-    for i in range(5):
         pag.hotkey('shift', '\t')
     pag.press('enter')
 
@@ -61,108 +54,70 @@ def person_filling(person=None):
     elif person == None:
         pass
     else:
-        raise ValueError('Person filling in not recognized.')
+        raise ValueError('Unrecognized person filling in.')
 
 
 
-def first_page(page, first_entry=True):
+def first_page(metadata, first_entry=True):
+    pscid, enterer, date = metadata
     activate_page()
-    pag.typewrite(page[0])
+    pag.typewrite(pscid)
     pag.press('\t')
-    pag.typewrite(page[1])
+    pag.typewrite(enterer)
     pag.press('\t')
-    if first_entry:
-        pag.typewrite(['space', '\t'])
-    else:
-        pag.typewrite(['down', '\t'])
-    pag.typewrite(page[2])
+    pag.typewrite([('down', 'space')[first_entry], '\t'])
+    pag.typewrite(date)
+#    pag.typewrite(page[0])
+#    pag.press('\t')
+#    pag.typewrite(page[1])
+#    pag.press('\t')
+#    if first_entry:
+#        pag.typewrite(['space', '\t'])
+#    else:
+#        pag.typewrite(['down', '\t'])
+#    pag.typewrite(page[2])
     next_page()
 
-def skip_cbcl():
-    for i in range(13):
+def skip(saq):
+    saqs = {'cbcl': 13, 
+            'sears': 4, 
+            'pss': 1, 
+            'srs': 6, 
+            'bsi': 5, 
+            'mspss': 2, 
+            'les': 16, 
+            'brief2': 7, 
+            'ace': 2}
+    for i in range(saqs[saq]):
         wait()
         next_page()
 
-def sears(pages, person=None):
-    if len(pages) != 3:
-        raise ValueError('Incorrect number of pages, expected 3.')
-    # Skip the first page.
-    wait()
-    next_page()
+def fill_pages(pages, person=None):
+    """ sears
+        pss
+        srs
+        bsi
+        mspss
+        ace
+    """
     for i, page in enumerate(pages):
-        if len(page) != 13:
-            raise ValueError
         fill_page(page)
-#        activate_page()
-#        for val in page:
-#            select_option(val)
-#            pag.press('\t')
-        if i < 2:
-            # Go to next page.
-            next_page()
-    person_filling(person)
-
-def pss(page, person=None):
-    if len(page) != 10:
-        raise ValueError('Incorrect number of values, expected 10.')
-    fill_page(page)
-#    activate_page()
-#    for val in page:
-#        select_option(val)
-#        pag.press('\t')
-    person_filling(person)
-
-def skip_pss():
-    wait()
-    next_page()
-
-def skip_srs():
-    for i in range(6):
-        wait()
+        if i == len(pages) - 1:
+            person_filling(person)
         next_page()
-
-def srs(pages, person=None):
-    if len(pages) != 6:
-        raise ValueError('Incorrect number of pages, expected 6.')
-    for i, page in enumerate(pages):
-        fill_page(page)
-#        activate_page()
-#        for val in page:
-#            select_option(val)
-#            pag.press('\t')
-        if i < 5:
-            next_page()
-    person_filling(person)
-
-def bsi(pages, person=None):
-    if len(pages) != 5:
-        raise ValueError('Incorrect number of pages, expected 5.')
-    for i, page in enumerate(pages):
-        fill_page(page)
-#        activate_page()
-#        for val in page:
-#            select_option(val)
-#            pag.press('\t')
-        if i < 4:
-            next_page()
-    person_filling(person)
-
-def mspss(pages, person=None):
-    if len(pages) != 2:
-        raise ValueError('Incorrect number of pages, expected 2.')
-    for i, page in enumerate(pages):
-        fill_page(page)
-#        activate_page()
-#        for val in page:
-#            select_option(val)
-#            pag.press('\t')
-        if i < 1:
-            next_page()
-    person_filling(person)
 
 def les(part1, part2, person=None):
-    if len(part1) != 7:
-        raise ValueError('Incorrect number of pages, expected 7.')
+    for i, (page1, page2) in enumerate(zip(part1, part2)):
+        activate_page()
+        for j, val in enumerate(page1):
+            select_option(val)
+            pag.press('\t')
+            if (i == 1 and j == 5) or (i == 3 and j == 6):
+                pag.press('\t')
+        if any(x != 1 for x in page1):
+            next_page()
+            fill_page(page2)
+
     for i in range(len(part1)):
         activate_page()
         for j in range(len(part1[i])):
@@ -173,56 +128,20 @@ def les(part1, part2, person=None):
         if any(x != 1 for x in part1[i]):
             next_page()
             fill_page(part2[i])
-#            activate_page()
-#            for val in part2[i]:
-#                select_option(val)
-#                pag.press('\t')
         if i < 6:
             next_page()
     next_page()
-    # Skip others.
     wait()
     next_page()
     activate_page()
-    person_filling(person)
-
-def skip_les():
-    for i in range(16):
-        wait()
-        next_page()
-
-def skip_brief2():
-    for i in range(7):
-        wait()
-        next_page()
-
-def skip_ace():
-    for i in range(2):
-        wait()
-        next_page()
-
-def skip_sears():
-    for i in range(4):
-        wait()
-        next_page()
-
-def ace(pages, person=None):
-    if len(pages) != 2:
-        raise ValueError('Incorrect number of pages, expected 2.')
-    for i, page in enumerate(pages):
-        fill_page(page)
-#        activate_page()
-#        for val in page:
-#            select_option(val)
-#            pag.press('\t')
-        if i < 1:
-            next_page()
     person_filling(person)
 
 def pcq():
     pass
 
 def skip_pcq():
+    """ There are still issues...
+    """
     for i in range(38):
         pag.press('end')
         print(f'Skipping page {i + 1}...')
@@ -249,42 +168,25 @@ def skip_pcq():
 
 
 def main():
-    page1 = ['010-20289', 'Roscoe', '16/04/2019']
-    first_page(page1, first_entry=True)
-    skip_cbcl()
-    
-##    skip_sears()
-##    skip_pss()
-##    skip_srs()
-        
-    sears39 = [[4, 3, 2, 4, 3, 3, 3, 3, 2, 2, 3, 3, 3],
+    metadata = ['010-20289', 'Roscoe', '16/04/2019']
+    sears39 = [[],
+               [4, 3, 2, 4, 3, 3, 3, 3, 2, 2, 3, 3, 3],
                [3, 2, 2, 3, 3, 3, 3, 2, 3, 1, 4, 2, 3],
                [2, 3, 2, 3, 2, 2, 2, 4, 2, 2, 3, 2, 2]]
-    sears(sears39, 'mother')
-    next_page()
-    pss10 = [1, 1, 3, 4, 4, 2, 3, 4, 3, 1]
-    pss(pss10, 'mother')
-    next_page()
+    pss10 = [[1, 1, 3, 4, 4, 2, 3, 4, 3, 1]]
     srs65 = [[4, 1, 4, 1, 2, 2, 3, 1, 2, 2, 4],
              [4, 2, 1, 4, 1, 1, 2, 2, 1, 4, 3],
              [1, 1, 2, 3, 2, 2, 1, 1, 2, 3, 1],
              [1, 1, 1, 1, 3, 2, 3, 1, 1, 4, 2],
              [3, 1, 2, 4, 2, 1, 1, 1, 1, 1, 3],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-    srs(srs65, None)
-    next_page()
     bsi53 = [[2, 1, 1, 2, 2, 2, 1, 1, 1, 5],
              [1, 2, 1, 2, 1, 1, 1, 2, 1, 5],
              [1, 1, 1, 3, 2, 4, 2, 1, 1, 1],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 5, 1, 1]]
-    bsi(bsi53, 'mother')
-    next_page()
     mspss12 = [[5, 5, 5, 5, 5, 5],
                [5, 5, 5, 5, 5, 5]]
-    mspss(mspss12, 'mother')
-    next_page()
-
     les47a = [[0, 1, 1, 0],
               [1, 1, 1, 1, 1, 1, 1],
               [0, 0, 1, 0, 1, 1, 1, 0, 0],
@@ -299,22 +201,36 @@ def main():
               [4, 4, 3, 6, 4, 4, 4],
               [5, 5, 4, 4, 4, 4, 4],
               [3, 3, 4]]
-    les(les47a, les47b, 'father')
-    next_page()
-
-##    skip_les()
-
-    skip_brief2()
-    
     ace10 = [[2, 2, 2, 2, 2],
              [1, 1, 2, 2, 1]]
-    ace(ace10, 'mother')
-    next_page()
-##    skip_ace()
-##    skip_pcq()
 
-    # Primary Caregiver Questionnaire
-    # This will be extremely challenging...
+    first_page(metadata, first_entry=True)
+    skip('cbcl')
+
+    fill_pages(sears39, 'mother')
+#    skip('sears')
+
+    fill_pages(pss10, 'mother')
+#    skip('pss')
+
+    fill_pages(srs65, None)
+#    skip('srs')
+
+    fill_pages(bsi53, 'mother')
+
+    fill_pages(mspss12, 'mother')
+
+    les(les47a, les47b, 'father')
+    next_page()
+#    skip_les()
+
+    skip_brief2()
+
+    fill_pages(ace10, 'mother')
+#    skip_ace()
+
+#    skip_pcq()
+
 
 if __name__ == '__main__':
     main()
