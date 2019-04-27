0433be99c0e1e8b254d62ff78c6775fc830dc04d
Roscoe <ahnouuyz@gmail.com>
2019-04-27 11:21:17 +0800

    Simple eval

--- parsing.py
+++ parsing.py
@@ -5,10 +5,7 @@ def tokenization(expr):
     tokens = []
     number = ''
     for char in expr:
-        if char == ' ':
-#            print('Discarding a space')
-            continue
-        elif char.isnumeric() or char == '.':
+        if char.isnumeric() or char == '.':
 #            print('Part of a number:', char)
             number += char
         elif char in symbols:
@@ -18,7 +15,7 @@ def tokenization(expr):
                 number = ''
             tokens.append(char)
         else:
-            raise ValueError('Unrecognized character:', char)
+            print('Unrecognized character:', char, '<-- has been ignored')
     return tokens
 
 def has_precedence(op1, op2):
@@ -34,16 +31,34 @@ def has_precedence(op1, op2):
     else:
         return False
 
+def operate(number1, operator, number2):
+    if operator == '+':
+        return number1 + number2
+    elif operator == '-':
+        return number1 - number2
+    elif operator == '*':
+        return number1 * number2
+    elif operator == '/':
+        return number1 / number2
+    elif operator == '^':
+        return pow(number1, number2)
+    else:
+        print('Unknown operator:', operator)
+
 def simple_evaluation(tokens):
     # Find the operator with the highest precedence.
     for i in range(1, len(tokens) - 3, 2):
         current_token = tokens[i]
         next_token = tokens[i + 2]
         print(current_token, next_token)
+        print(has_precedence(current_token, next_token))
+        if has_precedence(current_token, next_token):
+            print('We will evaluate here')
+            print(tokens[i - 1:i + 2])
+            print(operate(tokens[i - 1], tokens[i], tokens[i + 1]))
 #    operators = tokens[1::2]
 #    print(tokens)
 #    print(operators)
-    raise NotImplementedError
 
 def complex_evaluation(tokens):
     raise NotImplementedError
