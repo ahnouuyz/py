02936b00c0268d26f1b5bc40b62280baf00e47c4
Roscoe <45630405+roscoelai@users.noreply.github.com>
2019-07-23 13:51:55 +0800

    Update compare.sh

--- bash_basics/compare.sh
+++ bash_basics/compare.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-f1="set1"
-f2="set2"
+file1="set1"
+file2="set2"
 
-echo "Comparing 2 sets of about 1,000,000 entries each took about 35 sec on a 10-year-old computer."
+sorted_file1="${file1}.sorted"
+sorted_file2="${file2}.sorted"
+unique1="${file1}_unique.txt"
+unique2="${file2}_unique.txt"
+commons="commmons.txt"
+
+if [ ! -f $sorted_file1 ]; then
+	sort $file1 > $sorted_file1
+	echo "New file: $sorted_file1 created."
+fi
+if [ ! -f $sorted_file2 ]; then
+	sort $file2 > $sorted_file2
+	echo "New file: $sorted_file2 created."
+fi
+
+echo -e "\nThis takes about 43 seconds to compare 2 sets of about 4,000,000 entries each (includes sorting the files)."
+echo -e "This takes about 17 seconds if the files have been sorted.\n"
 echo "Entries unique to either file will (try to) be shown on the screen:"
-comm -3 --total <(tr '[:upper:]' '[:lower:]' <$f1 | sort) <(tr '[:upper:]' '[:lower:]' <$f2 | sort)
+comm -3 --total $sorted_file1 $sorted_file2
 
-echo "Now, unique and common values will be saved into separate files..."
-comm -23 <(tr '[:upper:]' '[:lower:]' <$f1 | sort) <(tr '[:upper:]' '[:lower:]' <$f2 | sort) > ${f1}_only
-comm -13 <(tr '[:upper:]' '[:lower:]' <$f1 | sort) <(tr '[:upper:]' '[:lower:]' <$f2 | sort) > ${f2}_only
-comm -12 <(tr '[:upper:]' '[:lower:]' <$f1 | sort) <(tr '[:upper:]' '[:lower:]' <$f2 | sort) > both
-echo "Done! 3 separate files saved!"
+diff --speed-large-files $file1 $file2 | tee >(grep "< " | tr -d "< " | sort > $unique1) >(grep "> " | tr -d "> " | sort > $unique2) > /dev/null
+echo "Values unique to $file1 written to $unique1"
+echo "Values unique to $file2 written to $unique2"
 
-# for x in $(cat $f1)
-# do
-# 	if [ $(grep -i $x $f2) ]
-# 	then
-# 		found="$x found in both"
-# 		printf "$found\r\n" >> both1.txt
-# 		echo $found
-# 	else
-# 		notfound="$x not found in $f2"
-# 		printf "$notfound\r\n" >> ${f2}_missing.txt
-# 	fi
-# done
+comm -12 $sorted_file1 $sorted_file2 > $commons
+echo "Values common to both $file1 and $file2 written to $commons"
