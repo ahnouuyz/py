c822506aaa1876b7e57d1c3f67adb43141aabe9c
Roscoe <45630405+roscoelai@users.noreply.github.com>
2019-08-26 00:12:48 +0800

    Update naive_nn.py

--- py_unsorted/naive_nn.py
+++ py_unsorted/naive_nn.py
@@ -4,33 +4,31 @@ import numpy as np
 
 class NeuralNet:
     def __init__(self, X, Y, inner_layers=[]):
+        np.random.seed(1)
         self.X = X
         self.Y = Y
         layers = [X.shape[1]] + inner_layers + [Y.shape[1]]
-#         np.random.seed(1)
         self.As = [X] + [np.zeros((X.shape[0], n)) for n in layers[1:]]
         self.Thetas = [np.random.rand(l, r) for l, r in zip(layers[:-1], layers[1:])]
     
-    def _back_propagation(self, g, alpha):
-        """ Still don't really understand what's going on here.
-            Therefore, the code is still a little untidy.
-        """
-        temp = 2 * (self.As[-1] - self.Y) * g(self.As[-1])
-        Ds = [self.As[-2].T @ temp]
-        for i, Theta in enumerate(self.Thetas[:0:-1]):
-            Al = self.As[-(3 + i)]
-            Ar = self.As[-(2 + i)]
-            temp = temp @ Theta.T * g(Ar)
-            Ds.append(Al.T @ temp)
-        for Theta, D in zip(self.Thetas, Ds[::-1]):
-            Theta -= alpha * D
-    
-    def _forward_propagation(self, g):
+    def _fprop(self, g):
         for i, Theta in enumerate(self.Thetas):
             self.As[i + 1] = g(self.As[i] @ Theta)
         self.output = self.As[-1]
     
-    def train(self, iterations, choice, alpha=0.5):
+    def _bprop(self, g, alpha):
+        d_layer = 2 * (self.output - self.Y) * g(self.output)
+        for i in range(len(self.Thetas)):
+            A_prev = self.As[-(2 + i)]
+            Theta = self.Thetas[-(1 + i)]
+            if i > 0:
+                Theta_next = self.Thetas[-i]
+                A = self.As[-(1 + i)]
+                d_layer = d_layer @ Theta_next.T * g(A)
+            d_theta = A_prev.T @ d_layer
+            Theta -= d_theta * (alpha / self.X.shape[0])
+    
+    def train(self, iterations, func, alpha=1):
         activations = {
             'sigmoid': lambda x: 1.0 / (1.0 + np.exp(-x)),
             'relu': lambda x: x * (x > 0),
@@ -39,26 +37,30 @@ class NeuralNet:
             'sigmoid': lambda x: x * (1.0 - x),
             'relu': lambda x: 1.0 * (x > 0),
         }
+        score = 0
         for i in range(iterations):
-            self._forward_propagation(activations[choice])
-            self._back_propagation(gradients[choice], alpha)
-            score = self.accuracy()
-            print(f'Iteration {i + 1}: accuracy = {score * 100:.2f}%')
-            if (1 - score) < 0.05:
+            if score < 0.99:
+                self._fprop(activations[func])
+                self._bprop(gradients[func], alpha)
+                if self.accuracy() != score:
+                    print(f'Iteration {i + 1}: accuracy = {self.accuracy() * 100:.2f}%')
+                    score = self.accuracy()
+            else:
                 break
+        else:
+            print(f'Iteration {i + 1}: accuracy = {self.accuracy() * 100:.2f}%')
     
     def accuracy(self):
-#         nwrong = ((self.output - self.Y) ** 2).round().sum()
-        nwrong = ((self.output.round() - self.Y) ** 2).sum()
-        return (1 - (nwrong / len(y)))
+        return (self.output.round() == self.Y).mean()
     
 if __name__ == '__main__':
-#     np.random.seed(1)
-    X = np.random.rand(100, 2)
-    Y = np.random.randint(0, 2, size=(100, 1))
+    np.random.seed(1234)
+    N = 40
+    X = np.random.rand(N, 2)
+    Y = np.random.randint(0, 2, size=(N, 1))
     
-    nn = NeuralNet(X, Y, inner_layers=[3, 3])
-    nn.train(iterations=100, choice='sigmoid', alpha=1)
+    nn = NeuralNet(X, Y, inner_layers=[3])
+    nn.train(iterations=1000, func='sigmoid', alpha=1)
     
     print()
     for i, Theta in enumerate(nn.Thetas):
