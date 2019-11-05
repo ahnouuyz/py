ca6ea0c327890312fe2ff0ad26de02b20f961aa8
Roscoe <45630405+roscoelai@users.noreply.github.com>
2019-11-05 17:06:37 +0800

    Update paths.py

--- py_unsorted/paths.py
+++ py_unsorted/paths.py
@@ -2,35 +2,30 @@ from typing import List, Tuple
 
 Point = Tuple[int, int]
 
-def steps_to_xy(x: int, y: int, forbidden: List[Point]=[]) -> int:
-    """ The number of steps to get to a certain point is simply the 
-        sum of the number of steps to reach the previous two points, 
-        unless one of the "previous points" is "outside" the board 
-        (does not exist), in which case "it" contributes 0 (the other 
-        point contributes as per normal). 
+def paths_to_xy(x: int, y: int, in_pts: List[Point]=[]) -> int:
+    """ Number of shortest paths to a point is the sum of the number of 
+        shortest paths to its parent points. 
         
-        The origin always contributes 1.
+        Exception:
+            There is only 1 shortest path to any point along the edges 
+            containing the origin.
         
-        Forbidden points, if any, are points on the inner grid, 
-        and contribute 0.
+        No paths can travel through inner grid points, so if a parent point 
+        happens to be an inner grid point, it contributes 0 paths to the sum.
         
         Arguments:
             x (int): final row
             y (int): final column
-            forbidden (list of tuples): list of points that the path cannot cross
+            in_pts (list of tuples): list of inner grid points
         
         Return the number of allowed paths from (0, 0) to (x, y).
     """
-    if (x, y) in forbidden:
+    if (x, y) in in_pts:
         return 0
-    elif x < 1 and y < 1:
+    elif x < 1 or y < 1:
         return 1
-    elif x < 1:
-        return steps_to_xy(x, y - 1, forbidden)
-    elif y < 1:
-        return steps_to_xy(x - 1, y, forbidden)
     else:
-        return steps_to_xy(x - 1, y, forbidden) + steps_to_xy(x, y - 1, forbidden)
+        return paths_to_xy(x - 1, y, in_pts) + paths_to_xy(x, y - 1, in_pts)
 
 def get_forbidden_points(x0: int, y0: int, dx: int, dy: int) -> List[Point]:
     """ Create a list of points covered by the inner grid. These will be the 
@@ -49,15 +44,13 @@ def get_forbidden_points(x0: int, y0: int, dx: int, dy: int) -> List[Point]:
 def valid_input(sizeGrid: int, inX: int, inSize: int) -> bool:
     if sizeGrid < 0:
         print('Error: Main grid must have positive size.')
-        return False
     elif (inX < 0) or (sizeGrid <= inX):
         print('Error: Inner grid origin cannot be at the top or right edges.')
-        return False
     elif sizeGrid < inX + inSize:
         print('Error: Inner grid must be within the main grid.')
-        return False
     else:
         return True
+    return False
 
 def nsp(sizeGrid: int, inX: int, inSize: int) -> int:
     """ Calculate the number of allowed shortest paths from (0, 0) to (n, n), 
@@ -66,7 +59,7 @@ def nsp(sizeGrid: int, inX: int, inSize: int) -> int:
         
         Arguments:
             sizeGrid (int): size of the main grid
-            inX (int): coordinates (repeated) of the bottom-left corner of the inner grid
+            inX (int): coordinates of the bottom-left corner of the inner grid
             inSize (int): size of the inner grid
         
         Return the number of paths.
@@ -75,7 +68,7 @@ def nsp(sizeGrid: int, inX: int, inSize: int) -> int:
         final_x = sizeGrid - 1
         final_y = sizeGrid - 1
         forbidden_points = get_forbidden_points(inX, inX, inSize, inSize)
-        return steps_to_xy(final_x, final_y, forbidden=forbidden_points)
+        return paths_to_xy(final_x, final_y, in_pts=forbidden_points)
     else:
         print('  Execution failed with the following parameters:')
         print(f'{"SizeGrid: " + str(sizeGrid):>16}')
@@ -89,10 +82,10 @@ if __name__ == '__main__':
         'nsp(5, 0, 1)', # 0
         'nsp(5, 4, 1)', # 0
         'nsp(5, 2, 0)', # 70
-        'nsp(-2, 1, 3)',
-        'nsp(2, 1, 3)',
-        'nsp(6, 6, 3)',
-    #     'nsp("a", 1, 3)',
+        'nsp(-2, 1, 3)', # error
+        'nsp(2, 1, 3)', # error
+        'nsp(6, 6, 3)', # error
+    #     'nsp("a", 1, 3)', # maybe next time...
     ]
 
     for test in tests:
